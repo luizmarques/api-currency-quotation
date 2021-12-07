b9c54fd57607aa5f0f446c8cc69481008a447201
LuizPC <luiz.marques@al.infnet.edu.br>
2021-12-07 17:45:42 -0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express, { Router, Request, Response } from 'express';
+
+const app = express();
+
+const router = Router();
+
+app.use(express.json());
+
+router.get('/', (request: Request, response: Response) => {
+    response.json({ message: 'Hello World: from Express + TypeScript' });
+});
+
+app.use(router);
+
+app.listen(3003, () => console.log('Server running on port 3003'));
